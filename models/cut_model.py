36a6367d6823672498488d9b08e2230bdf71839c
LTTdouble <130349905+LTTdouble@users.noreply.github.com>
2026-03-11 09:50:33 +0800

    first commit
    first commit

---
+++ models/cut_model.py
@@ -0,0 +1,204 @@
+import numpy as np
+import torch
+from .base_model import BaseModel
+from . import networks
+from .patchnce import PatchNCELoss
+import util.util as util
+
+
+class CUTModel(BaseModel):
+    """ This class implements CUT and FastCUT model. """
+
+    @staticmethod
+    def modify_commandline_options(parser, is_train=True):
+        parser.add_argument('--CUT_mode', type=str, default="CUT", choices='(CUT, cut, FastCUT, fastcut)')
+        parser.add_argument('--lambda_GAN', type=float, default=1.0, help='weight for GAN loss: GAN(G(X))')
+        parser.add_argument('--lambda_NCE', type=float, default=1.0, help='weight for NCE loss: NCE(G(X), X)')
+        parser.add_argument('--nce_idt', type=util.str2bool, nargs='?', const=True, default=False,
+                            help='use NCE loss for identity mapping: NCE(G(Y), Y))')
+
+        parser.add_argument('--nce_layers', type=str, default='0,2,3,4,5', help='compute NCE loss on which layers')
+
+        parser.add_argument('--nce_includes_all_negatives_from_minibatch',
+                            type=util.str2bool, nargs='?', const=True, default=False,
+                            help='(used for single image translation) If True, include the negatives from the other samples of the minibatch.')
+        parser.add_argument('--netF', type=str, default='mlp_sample', choices=['sample', 'reshape', 'mlp_sample'],
+                            help='how to downsample the feature map')
+        parser.add_argument('--netF_nc', type=int, default=256)
+        parser.add_argument('--nce_T', type=float, default=0.07, help='temperature for NCE loss')
+        parser.add_argument('--num_patches', type=int, default=256, help='number of patches per layer')
+        parser.add_argument('--flip_equivariance',
+                            type=util.str2bool, nargs='?', const=True, default=False,
+                            help="Enforce flip-equivariance as additional regularization.")
+
+        parser.set_defaults(pool_size=0)  # no image pooling
+        opt, _ = parser.parse_known_args()
+
+        if opt.CUT_mode.lower() == "cut":
+            parser.set_defaults(nce_idt=True, lambda_NCE=1.0)
+        elif opt.CUT_mode.lower() == "fastcut":
+            parser.set_defaults(nce_idt=False, lambda_NCE=10.0, flip_equivariance=True,
+                                n_epochs=150, n_epochs_decay=50)
+        else:
+            raise ValueError(opt.CUT_mode)
+        return parser
+
+    def __init__(self, opt):
+        BaseModel.__init__(self, opt)
+        self.loss_names = ['G_GAN', 'D_real', 'D_fake', 'G', 'NCE']
+        self.visual_names = ['real_A', 'fake_B', 'real_B']
+        self.nce_layers = [int(i) for i in self.opt.nce_layers.split(',')]
+
+        if opt.nce_idt and self.isTrain:
+            self.loss_names += ['NCE_Y']
+            self.visual_names += ['idt_B']
+
+        if self.isTrain:
+            self.model_names = ['G', 'F', 'D']
+        else:
+            self.model_names = ['G', 'F']
+
+        self.netG = networks.define_G(opt.input_nc, opt.output_nc, opt.ngf, opt.netG, opt.normG,
+                                      not opt.no_dropout, opt.init_type, opt.init_gain,
+                                      opt.no_antialias, opt.no_antialias_up, self.gpu_ids, opt)
+        self.netF = networks.define_F(opt.input_nc, opt.netF, opt.normG,
+                                      not opt.no_dropout, opt.init_type, opt.init_gain,
+                                      opt.no_antialias, self.gpu_ids, opt)
+
+        if self.isTrain:
+            self.netD = networks.define_D(opt.output_nc, opt.ndf, opt.netD, opt.n_layers_D, opt.normD,
+                                          opt.init_type, opt.init_gain, opt.no_antialias, self.gpu_ids, opt)
+
+            self.criterionGAN = networks.GANLoss(opt.gan_mode).to(self.device)
+            self.criterionNCE = []
+            for nce_layer in self.nce_layers:
+                self.criterionNCE.append(PatchNCELoss(opt).to(self.device))
+
+            self.criterionIdt = torch.nn.L1Loss().to(self.device)
+            self.optimizer_G = torch.optim.Adam(self.netG.parameters(), lr=opt.lr, betas=(opt.beta1, opt.beta2))
+            self.optimizer_D = torch.optim.Adam(self.netD.parameters(), lr=opt.lr, betas=(opt.beta1, opt.beta2))
+            self.optimizers.append(self.optimizer_G)
+            self.optimizers.append(self.optimizer_D)
+
+    def data_dependent_initialize(self, data):
+        bs_per_gpu = data["A"].size(0) // max(len(self.opt.gpu_ids), 1)
+        self.set_input(data)
+        self.real_A = self.real_A[:bs_per_gpu]
+        self.real_B = self.real_B[:bs_per_gpu]
+        self.forward()
+        if self.opt.isTrain:
+            self.compute_D_loss().backward()
+            self.compute_G_loss().backward()
+            self.optimizer_F = torch.optim.Adam(self.netF.parameters(), lr=self.opt.lr,
+                                                betas=(self.opt.beta1, self.opt.beta2))
+            self.optimizers.append(self.optimizer_F)
+
+    def optimize_parameters(self):
+        self.forward()
+        self.set_requires_grad(self.netD, True)
+        self.optimizer_D.zero_grad()
+        self.loss_D = self.compute_D_loss()
+        self.loss_D.backward()
+        self.optimizer_D.step()
+
+        self.set_requires_grad(self.netD, False)
+        self.optimizer_G.zero_grad()
+        if self.opt.netF == 'mlp_sample':
+            self.optimizer_F.zero_grad()
+        self.loss_G = self.compute_G_loss()
+        self.loss_G.backward()
+        self.optimizer_G.step()
+        if self.opt.netF == 'mlp_sample':
+            self.optimizer_F.step()
+
+    def set_input(self, input):
+        AtoB = self.opt.direction == 'AtoB'
+        self.real_A = input['A' if AtoB else 'B'].to(self.device)
+        self.real_B = input['B' if AtoB else 'A'].to(self.device)
+        self.image_paths = input['A_paths' if AtoB else 'B_paths']
+
+    def forward(self):
+        """Run forward pass; called by both functions <optimize_parameters> and <test>."""
+        # 1. Prepare input
+        self.real = torch.cat((self.real_A, self.real_B),
+                              dim=0) if self.opt.nce_idt and self.opt.isTrain else self.real_A
+
+        if self.opt.flip_equivariance:
+            self.flipped_for_equivariance = self.opt.isTrain and (np.random.random() < 0.5)
+            if self.flipped_for_equivariance:
+                self.real = torch.flip(self.real, [3])
+
+        # 2. Generator forward pass
+        self.fake = self.netG(self.real)
+
+        # 3. [Critical fix] Handle the issue of Deep Supervision returning a list
+        # If it returns a List, take the last one (Main Output)
+        if isinstance(self.fake, list):
+            self.fake_final = self.fake[-1]
+        else:
+            self.fake_final = self.fake
+
+        # 4. Split Batch
+        self.fake_B = self.fake_final[:self.real_A.size(0)]
+        if self.opt.nce_idt:
+            self.idt_B = self.fake_final[self.real_A.size(0):]
+
+    def compute_D_loss(self):
+        """Calculate GAN loss for the discriminator"""
+        fake = self.fake_B.detach()
+        pred_fake = self.netD(fake)
+        self.loss_D_fake = self.criterionGAN(pred_fake, False).mean()
+        self.pred_real = self.netD(self.real_B)
+        loss_D_real = self.criterionGAN(self.pred_real, True)
+        self.loss_D_real = loss_D_real.mean()
+        self.loss_D = (self.loss_D_fake + self.loss_D_real) * 0.5
+        return self.loss_D
+
+    def compute_G_loss(self):
+        """Calculate GAN and NCE loss for the generator"""
+        fake = self.fake_B
+        # 1. GAN Loss
+        if self.opt.lambda_GAN > 0.0:
+            pred_fake = self.netD(fake)
+            self.loss_G_GAN = self.criterionGAN(pred_fake, True).mean() * self.opt.lambda_GAN
+        else:
+            self.loss_G_GAN = 0.0
+
+        # 2. NCE Loss
+        if self.opt.lambda_NCE > 0.0:
+            self.loss_NCE = self.calculate_NCE_loss(self.real_A, self.fake_B)
+        else:
+            self.loss_NCE, self.loss_NCE_bd = 0.0, 0.0
+
+        # 3. Identity Loss
+        if self.opt.nce_idt and self.opt.lambda_NCE > 0.0:
+            self.loss_NCE_Y = self.calculate_NCE_loss(self.real_B, self.idt_B)
+            loss_NCE_both = (self.loss_NCE + self.loss_NCE_Y) * 0.5
+        else:
+            loss_NCE_both = self.loss_NCE
+
+        self.loss_G = self.loss_G_GAN + loss_NCE_both
+        return self.loss_G
+
+    def calculate_NCE_loss(self, src, tgt):
+        n_layers = len(self.nce_layers)
+
+        # 1. Query features (Fake) - pass encode_only=True
+        feat_q = self.netG(tgt, self.nce_layers, encode_only=True)
+
+        if self.opt.flip_equivariance and self.flipped_for_equivariance:
+            feat_q = [torch.flip(fq, [3]) for fq in feat_q]
+
+        # 2. Key features (Real) - pass encode_only=True
+        feat_k = self.netG(src, self.nce_layers, encode_only=True)
+
+        # 3. MLP sampling
+        feat_k_pool, sample_ids = self.netF(feat_k, self.opt.num_patches, None)
+        feat_q_pool, _ = self.netF(feat_q, self.opt.num_patches, sample_ids)
+
+        total_nce_loss = 0.0
+        for f_q, f_k, crit, nce_layer in zip(feat_q_pool, feat_k_pool, self.criterionNCE, self.nce_layers):
+            loss = crit(f_q, f_k) * self.opt.lambda_NCE
+            total_nce_loss += loss.mean()
+
+        return total_nce_loss / n_layers
---
+++ models/patchnce.py
@@ -0,0 +1,222 @@
+
+# ///////////////////////////////////////////////////////////////////////////////  ArcFace + Hard Negative Mining
+
+
+
+# import torch
+# import torch.nn as nn
+# import torch.nn.functional as F
+# from packaging import version
+# import math
+#
+# class PatchNCELoss(nn.Module):
+#     def __init__(self, opt):
+#         super().__init__()
+#         self.opt = opt
+#         self.cross_entropy_loss = torch.nn.CrossEntropyLoss(reduction='none')
+#         self.mask_dtype = torch.uint8 if version.parse(torch.__version__) < version.parse('1.2.0') else torch.bool
+#
+#         # ===========================================================
+#         # 1. Hyperparameters
+#         # ===========================================================
+#         self.margin = 0.2
+#         self.eps = 1e-6
+#         self.nce_T = getattr(opt, 'nce_T', 0.07)
+#         self.num_hard_negatives = getattr(opt, 'num_hard_negatives', 200)
+#
+#     def forward(self, feat_q, feat_k):
+#         """
+#         feat_q: Query features, shape: (Batch * Num_Patches, Dim)
+#         feat_k: Key features, shape: (Batch * Num_Patches, Dim)
+#         """
+#         # -----------------------------------------------------------
+#         # 1. L2 Normalize features
+#         # -----------------------------------------------------------
+#         feat_q = F.normalize(feat_q, p=2, dim=1)
+#         feat_k = F.normalize(feat_k, p=2, dim=1)
+#
+#         # -----------------------------------------------------------
+#         # 2. Batch dimension handling
+#         # -----------------------------------------------------------
+#         batchSize = self.opt.batch_size
+#
+#         if feat_q.shape[0] % batchSize == 0:
+#             current_batch_size = batchSize
+#             npatches = feat_q.shape[0] // batchSize
+#         else:
+#             current_batch_size = 1
+#             npatches = feat_q.shape[0]
+#
+#         dim = feat_q.shape[1]
+#         feat_k = feat_k.detach()
+#
+#         # ===========================================================
+#         # 3. Positive pairs: ArcFace + Easy Margin
+#         # ===========================================================
+#         # (A) Compute original Cosine Similarity
+#         cos_theta = F.cosine_similarity(feat_q, feat_k, dim=1)
+#         cos_theta = cos_theta.view(feat_q.shape[0], 1)
+#
+#         # (B) Numerical stability clamp
+#         cos_theta = torch.clamp(cos_theta, -1.0 + self.eps, 1.0 - self.eps)
+#
+#         # (C) Apply ArcFace logic
+#         if self.margin > 0.0:
+#             theta = torch.acos(cos_theta)
+#             theta_m = theta + self.margin
+#
+#             # Easy Margin protection strategy
+#             mask = (theta_m < math.pi)
+#             l_pos_arc = torch.cos(theta_m)
+#
+#             # Combine: ArcFace for easy samples, CosFace for hard samples
+#             l_pos = torch.where(mask, l_pos_arc, cos_theta - self.margin * 0.5)
+#         else:
+#             # Fallback to standard NCE if margin is 0
+#             l_pos = cos_theta
+#
+#         # ===========================================================
+#         # 4. Negative Pairs calculation
+#         # ===========================================================
+#         feat_q_view = feat_q.view(current_batch_size, npatches, dim)
+#         feat_k_view = feat_k.view(current_batch_size, npatches, dim)
+#
+#         l_neg_curbatch = torch.bmm(feat_q_view, feat_k_view.transpose(2, 1))
+#
+#         # Mask out self-comparisons
+#         diagonal = torch.eye(npatches, device=feat_q.device, dtype=self.mask_dtype)[None, :, :]
+#         l_neg_curbatch.masked_fill_(diagonal, -float('inf'))
+#
+#         l_neg = l_neg_curbatch.view(-1, npatches)
+#
+#         # ===========================================================
+#         # 5. Hard Negative Mining
+#         # ===========================================================
+#         l_neg_sorted, _ = torch.sort(l_neg, dim=1, descending=True)
+#
+#         num_neg = min(self.num_hard_negatives, npatches - 1)
+#         l_neg_hard = l_neg_sorted[:, :num_neg]
+#
+#         # ===========================================================
+#         # 6. Compute Final Loss
+#         # ===========================================================
+#         out = torch.cat((l_pos, l_neg_hard), dim=1)
+#         out = out / self.nce_T
+#
+#         labels = torch.zeros(out.size(0), dtype=torch.long, device=feat_q.device)
+#         loss = self.cross_entropy_loss(out, labels)
+#
+#         return loss
+#
+
+
+
+
+
+# ///////////////////////////////////////////////////////////  Simple Margin + Hard Negative Mining
+
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from packaging import version
+
+class PatchNCELoss(nn.Module):
+    def __init__(self, opt):
+        super().__init__()
+        self.opt = opt
+
+        self.cross_entropy_loss = torch.nn.CrossEntropyLoss(reduction='none')
+
+        # Compatibility for boolean mask across PyTorch versions
+        self.mask_dtype = torch.uint8 if version.parse(torch.__version__) < version.parse('1.2.0') else torch.bool
+
+        # ===========================================================
+        # Hyperparameters
+        # ===========================================================
+        self.margin = 0.2
+        self.nce_T = getattr(opt, 'nce_T', 0.07)
+        self.num_hard_negatives = getattr(opt, 'num_hard_negatives', 200)
+
+    def forward(self, feat_q, feat_k):
+        """
+        feat_q: Query features, shape: (Batch * Num_Patches, Dim)
+        feat_k: Key features, shape: (Batch * Num_Patches, Dim)
+        """
+        # -----------------------------------------------------------
+        # 1. L2 Normalize features
+        # -----------------------------------------------------------
+        feat_q = F.normalize(feat_q, p=2, dim=1)
+        feat_k = F.normalize(feat_k, p=2, dim=1)
+
+        # -----------------------------------------------------------
+        # 2. Batch dimension handling
+        # -----------------------------------------------------------
+        batchSize = self.opt.batch_size
+
+        # Infer current batch size and number of patches
+        if feat_q.shape[0] % batchSize == 0:
+            current_batch_size = batchSize
+            npatches = feat_q.shape[0] // batchSize
+        else:
+            # Fallback for incomplete batches or test time
+            current_batch_size = 1
+            npatches = feat_q.shape[0]
+
+        dim = feat_q.shape[1]
+        feat_k = feat_k.detach()
+
+        # -----------------------------------------------------------
+        # 3. Positive pairs calculation
+        # -----------------------------------------------------------
+        l_pos = F.cosine_similarity(feat_q, feat_k, dim=1)
+        l_pos = l_pos.view(feat_q.shape[0], 1)
+
+        # -----------------------------------------------------------
+        # 4. Negative pairs calculation
+        # -----------------------------------------------------------
+        feat_q_view = feat_q.view(current_batch_size, npatches, dim)
+        feat_k_view = feat_k.view(current_batch_size, npatches, dim)
+
+        # Batch matrix multiplication for all patch combinations
+        l_neg_curbatch = torch.bmm(feat_q_view, feat_k_view.transpose(2, 1))
+
+        # -----------------------------------------------------------
+        # 5. Mask diagonal entries (self-comparisons)
+        # -----------------------------------------------------------
+        diagonal = torch.eye(npatches, device=feat_q.device, dtype=self.mask_dtype)[None, :, :]
+        l_neg_curbatch.masked_fill_(diagonal, -float('inf'))
+
+        l_neg = l_neg_curbatch.view(-1, npatches)
+
+        # -----------------------------------------------------------
+        # 6. Hard Negative Mining
+        # -----------------------------------------------------------
+        # Sort in descending order to find the hardest negatives
+        l_neg_sorted, _ = torch.sort(l_neg, dim=1, descending=True)
+        num_neg = min(self.num_hard_negatives, npatches - 1)
+        l_neg_hard = l_neg_sorted[:, :num_neg]
+
+        # -----------------------------------------------------------
+        # 7. Apply Margin and compute Loss
+        # -----------------------------------------------------------
+        # Numerical stability clamp
+        l_pos = torch.clamp(l_pos, -1.0, 1.0)
+
+        # Apply margin to positive similarity
+        l_pos = l_pos - self.margin
+
+        # Concatenate logits: [Positive(1), Hard_Negatives(K)]
+        logits = torch.cat((l_pos, l_neg_hard), dim=1)
+        logits = logits / self.nce_T
+
+        # Compute CrossEntropyLoss (target is class 0)
+        labels = torch.zeros(logits.size(0), dtype=torch.long, device=feat_q.device)
+        loss = self.cross_entropy_loss(logits, labels)
+
+        return loss
+
+
+
+
+
